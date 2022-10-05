22f6d31d13692e5f37e174c738eeae2a51908a4c
aliasadullah2001 <ali.asadullah2001@gmail.com>
2022-10-05 17:25:22 -0400

    updateddd

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
-
+var path = require("path");
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    let resText = "<h2>Declaration(Instruction: text size in heading 2)</h2> <br>";
+    resText += "<a> instruction: The rest text is displayed in paragraph  as shown in screenshot. </a><br><br>";
+    resText += "<a> I declare that this test is my own work in accordance with Seneca Academic Policy. No part of this has<br> been copied manually or electronically from any other source.</a> <br><br>";
+    resText += "<a> Name:</a><b><mark> Ali Asadullah</b></mark><br> <br>";
+    resText += "<a> Student Number:</a><mark><b>174606210</b></mark><br> <br>";
+    resText += "<a href = './test1'> Go to Test1 </a><br> <br>";
+    resText += "<a> Instruction: Test1 is a link, which directs users to the route test1</a> <br>";
+    
+    res.send(resText);
+});
+app.get("/test1", function (req, res){
+    var resText = "<mark><h2> Good Luck! </h2></mark> <br>";
+    resText += "<a> instruction: text Good luck! in the heading 1. Text color is red, and background color is yellow.</a><br><br>";
+    resText += "<a href='/'> <h3> Go home </h3><br><br>";
+    resText += "<a> instruction: Go Home is in heading 3. It is hyperlink directing user to the home route.</a><br><br>";
+    res.send(resText);
 });
 
+
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, onHttpStart);
